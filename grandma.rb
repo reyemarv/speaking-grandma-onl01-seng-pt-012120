ce631fe2d719a7d19f384d2c317cc258c350f179
Marvin Reyes <reyemarv@gmail.com>
2020-02-01 05:00:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?" || "WHAT?"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
    "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT?"
+   "NO< NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
