0394fee531095a2108d87fb1f94451e2be16737c
Marvin Reyes <reyemarv@gmail.com>
2020-02-01 05:00:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "WHAT DID YOU EAT TODAY?"
    "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
-   "NO< NOT SINCE 1938!"
+   "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
