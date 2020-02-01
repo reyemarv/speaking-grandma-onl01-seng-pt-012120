89282c6ade15fc749c02d2e18c2fd291230e4c28
Marvin Reyes <reyemarv@gmail.com>
2020-02-01 05:07:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase !== phrase.uppercase()
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == "WHAT DID YOU EAT TODAY?"
    "NO, NOT SINCE 1938!"
